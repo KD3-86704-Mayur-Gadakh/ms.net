81a2d8599d777c0d1e3fda0123b6b209b6a2884b
Mayur Gadakh <mayurgadakh07@gmail.com>
2024-12-28 15:01:36 +0530

    Assignment No 1 Submited

---
+++ Question2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using System.Dynamic;
+
+namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World");
+            //Console.WriteLine(args[0]);
+            //Console.WriteLine(args[1]);
+
+            int a = int.Parse(args[0]);
+            int b = int.Parse(args[1]);
+            String operand = args[2];
+
+            switch (operand)
+            {
+                case "+":
+                    Console.WriteLine(a + b);
+                    break;
+
+                case "-":Console.WriteLine(a - b);
+                    break;
+
+                case "*":
+                    Console.WriteLine(a * b);
+                    break;
+                case "/":
+                    Console.WriteLine(a / b);
+                    break;
+
+                default:Console.WriteLine("Invalid Argument");
+                    break;
+
+
+
+                
+
+            }
+
+        }
+    }
+}
---
+++ Question3/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Question3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = int.Parse(args[0]);
+            int b = int.Parse(args[1]);
+            String operand = args[2];
+            int c=int.Parse(args[3]);
+            do
+            {
+                switch (operand) {
+
+                    case "+":
+                        Console.WriteLine(a+b);
+                        break;
+                        case "-":
+                        Console.WriteLine(a-b);
+                        break;
+                        case "*":
+                        Console.WriteLine(a*b);
+                        break;
+                        case "/":
+                        Console.WriteLine(a/b);
+                        break;
+                }
+
+            }while (c!=0);
+            
+        }
+    }
+}
